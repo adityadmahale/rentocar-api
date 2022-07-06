db3efc405f3c8e5f4dbbf3260be56a42d2e5ff0b
Gandhi Rajan Mahendran <gn479247@dal.ca>
2022-07-06 16:28:28 -0300

    created user model

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+  isAdmin: Boolean,
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, username: this.username, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports.User = User;
+
+module.exports.validate = (object) => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(50).required(),
+    email: Joi.string()
+      .min(5)
+      .max(50)
+      .email({ tlds: { allow: false } })
+      .required(),
+    password: Joi.string().min(5).max(50).required(),
+  });
+  return schema.validate(object);
+};
