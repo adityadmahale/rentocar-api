e338f784886e2d217d6cb13d5c875fcea758b7b1
Shaik Asaduddin <shaikasaduddin@Shaiks-MacBook-Pro-2.local>
2022-07-14 02:15:35 -0300

    feat(API): INtegarted stations code with vehicle management code changes

---
+++ routers/stations.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const { Station, validate } = require("../models/station");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        const stations = await Station.find();
+        res.status(200).json(stations);
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal server error",
+            success: false,
+        });
+    }
+});
+
+router.post("/", [auth, admin], async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).json(error.details[0].message);
+
+        const station = new Station({
+            stationName: req.body.stationName,
+            stationCode: req.body.stationCode,
+            address: req.body.address,
+            capacity: req.body.capacity
+        });
+        await station.save();
+
+        res.status(201).json(station);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({
+            message: "Internal server error",
+            success: false,
+        });
+    }
+});
+
+router.put("/:id", [auth, admin], async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).json(error.details[0].message);
+        const station = await Station.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    stationName: req.body.stationName,
+                    stationCode: req.body.stationCode,
+                    address: req.body.address,
+                    capacity: req.body.capacity
+                },
+            },
+            { new: true }
+        );
+        if (!station)
+            return res
+                .status(404)
+                .json(`The station with the code ${req.params.stationCode} was not found.`);
+
+        res.status(200).json(station);
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal server error",
+            success: false,
+        });
+    }
+});
+
+router.delete("/:id", [auth, admin], async (req, res) => {
+    try {
+        const station = await Station.findByIdAndRemove(req.params.id);
+        if (!station)
+            return res
+                .status(404)
+                .json(`The station with the code ${req.params.id} was not found.`);
+        res.status(200).json(station);
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal server error",
+            success: false,
+        });
+    }
+});
+
+module.exports = router;
